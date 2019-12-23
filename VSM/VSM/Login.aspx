023ee9ddbaf8f02e301ee11ecb560660b177dbd4
shreypateliya <57733512+shreypateliya@users.noreply.github.com>
2019-12-23 14:41:44 +0530

    Add files via upload

---
+++ VSM/VSM/Login.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="VSM.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Login</h1>
+            <asp:TextBox ID="txtmail" runat="server" placeholder="E-mail"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtpass" runat="server" TextMode="password" placeholder="Password"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" /><br />
+            <asp:Label ID="LoginError" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ VSM/VSM/Login.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using VSM.DB;
+
+namespace VSM
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        connection con = new connection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(con.userExist(txtmail.Text, txtpass.Text))
+            {
+                Session["name"] = con.name;
+                Session["type"] = con.type;
+                LoginError.Text = "user found";
+            }
+            else
+            {
+                LoginError.Text = "User Not found";
+            }
+            Response.Write(con.err);
+        }
+    }
+}

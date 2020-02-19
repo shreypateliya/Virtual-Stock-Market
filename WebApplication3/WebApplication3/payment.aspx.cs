1af583db1feb8f5ff128d07727bf941cfade3f9d
shikar <shahshikhar300@gmail.com>
2020-02-19 09:52:25 +0530

    webapp3 readded

---
+++ WebApplication3/WebApplication3/payment.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = 1;
+            for (i = 1; i <= 12; i++)
+            {
+                 Convert.ToString(i);
+            }
+        }
+    }
+}
